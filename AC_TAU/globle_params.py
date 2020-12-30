e7fad73096d68dd81bafaefd97bd31d9a9e67eb1
GaryBigpig <fegang2002@163.com>
2020-12-30 20:52:56 +0800

    ADD

---
+++ AC_TAU/globle_params.py
@@ -0,0 +1,6 @@
+
+
+TAU_PATH='/home/fenggang/Desktop/Files/Data/TAU-urban-acoustic-scenes-2019-mobile-development'
+TAU_METAFILE='/home/fenggang/Desktop/Files/Data/TAU-urban-acoustic-scenes-2019-mobile-development/meta.csv'
+
+MFCC_FEATURE='./data/MFCC_Feature/MFCC_Feature.npy'
